6286c1dfefe0e904f36b1b24712dcd30aa40d764
eleven <yinji@taido.com.cn>
2025-06-30 10:48:27 +0800

    pref(config): 调整配置

--- src/main/config/config.js
+++ src/main/config/config.js
@@ -4,22 +4,16 @@ import os from 'os'
 const isDev = process.env.NODE_ENV === 'development'
 const isWin = process.platform === 'win32'
 
+// 服务端IP和端口
 export const serviceUrl = {
-  face2face: isDev ? 'http://192.168.4.204:8383/easy' : 'http://127.0.0.1:8383/easy',
-  tts: isDev ? 'http://192.168.4.204:18180' : 'http://127.0.0.1:18180'
+  face2face: isDev ? 'http://8.130.32.64:8383/easy' : 'http://8.130.32.64:8383/easy',
+  tts: isDev ? 'http://8.130.32.64:18180' : 'http://8.130.32.64:18180'
 }
 
+// Samba目录配置
 export const assetPath = {
-  model: isWin
-    ? path.join('D:', 'heygem_data', 'face2face', 'temp')
-    : path.join(os.homedir(), 'heygem_data', 'face2face', 'temp'), // 模特视频
-  ttsProduct: isWin
-    ? path.join('D:', 'heygem_data', 'face2face', 'temp')
-    : path.join(os.homedir(), 'heygem_data', 'face2face', 'temp'), // TTS 产物
-  ttsRoot: isWin
-    ? path.join('D:', 'heygem_data', 'voice', 'data')
-    : path.join(os.homedir(), 'heygem_data', 'voice', 'data'), // TTS服务根目录
-  ttsTrain: isWin
-    ? path.join('D:', 'heygem_data', 'voice', 'data', 'origin_audio')
-    : path.join(os.homedir(), 'heygem_data', 'voice', 'data', 'origin_audio') // TTS 训练产物
+  model: path.join('K:', 'heygem_data', 'face2face', 'temp'), // 模特视频
+  ttsProduct: path.join('K:', 'heygem_data', 'face2face', 'temp'), //  TTS 产物
+  ttsRoot: path.join('K:', 'heygem_data', 'voice', 'data'), // TTS服务根目录
+  ttsTrain: path.join('K:', 'heygem_data', 'voice', 'data','origin_audio') // TTS 训练产物
 }
